a71598326b3dd00be919f7c677b8808121850961
IvanBecerraA <ivanbecerra91@gmail.com>
2023-11-07 14:12:08 -0300

    Agregar archivos de proyecto.

---
+++ BitAnalytics/Migrations/20231107143650_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BitAnalytics.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Plate = table.Column<string>(type: "TEXT", nullable: true),
+                    Make = table.Column<string>(type: "TEXT", nullable: true),
+                    Model = table.Column<string>(type: "TEXT", nullable: true),
+                    Color = table.Column<string>(type: "TEXT", nullable: true),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    Displacement = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
---
+++ BitAnalytics/Models/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitAnalytics.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo Patente es obligatorio.")]
+
+        public string Plate { get; set; }
+        [Required(ErrorMessage = "El campo Marca es obligatorio.")]
+        public string Make { get; set; }
+        [Required(ErrorMessage = "El campo Modelo es obligatorio.")]
+
+        public string Model { get; set; }
+        [Required(ErrorMessage = "El campo Color es obligatorio.")]
+
+        public string Color { get; set; }
+        [Required(ErrorMessage = "El campo Año es obligatorio.")]
+
+        public int Year { get; set; }
+        [Required(ErrorMessage = "El campo Cilindrada es obligatorio.")]
+
+        public int Displacement { get; set; }
+    }
+}
